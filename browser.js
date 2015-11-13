3681cf0211cb48aceac621f8d6256e60269e687a
Andy Bas <andyjbas@gmail.com>
2015-11-13 14:25:22 +0700

    reference included jquery instead of from node_modules

--- browser.js
+++ browser.js
@@ -3,7 +3,7 @@
 const notification = require('./lib/notification');
 
 window.onload = function() {
-  window.$ = window.jQuery = require('jquery');
+  window.$ = window.jQuery = require('./jquery.js');
   notification.init();
   notification.notify();
   setInterval(notification.query, 500);
